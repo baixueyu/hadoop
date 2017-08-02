ccfc7ef83bb642b84b71800f50fcca8e6acd06a2
gaosibei <gaosb@inspur.com>
2017-08-02 11:01:28 +0800

    fix s3a can not download 5G large file bug

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AInputStream.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AInputStream.java
@@ -86,7 +86,9 @@ public class S3AInputStream extends FSInputStream {
     LOG.debug("Actually opening file " + key + " at pos " + pos);
 
     GetObjectRequest request = new GetObjectRequest(bucket, key);
-    //request.setRange(pos, contentLength-1);
+    if (contentLength > 0) {
+      request.setRange(pos, contentLength-1);    	
+    }
 
     wrappedStream = client.getObject(request).getObjectContent();
 
